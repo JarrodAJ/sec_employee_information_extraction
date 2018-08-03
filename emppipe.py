f9f12d54687421460c20995cf49ff312755ff098
healthgradient <healthgradient@users.noreply.github.com>
2018-08-03 16:11:08 -0500

    Updated script files

--- emppipe.py
+++ emppipe.py
@@ -98,7 +98,7 @@ class EmpTypeRecognizer(object):
         return any([t._.get('is_full_time') for t in tokens])
 
 part_time_terms = get_case_combos(["half-time", "half time", "part-time", "part time"])
-full_time_terms = get_case_combos(["full-time", "full time", "40-hour equivalent", "40 hour equivalent", "full-time equivalent", "full time equivalent"])
+full_time_terms = get_case_combos(["full-time", "full time", "40-hour equivalent", "40 hour equivalent", "full-time equivalent", "full time equivalent", "full-"])
 emp_type_dict = {'PART_TIME': part_time_terms, 
                 'FULL_TIME': full_time_terms}
 
@@ -164,7 +164,7 @@ class EmpNounRecognizer(object):
 
 emp_terms_list = ["associates", "employees", "equivalents", "FTEs", "FTE's", "headcount", "individuals", 
                   "people", "persons", "team members", "workers", "workforce"]
-emp_terms_case_combos = get_case_combos(emp_terms_list)
+emp_terms_list = get_case_combos(emp_terms_list)
 
 class NumberWordRecognizer(object):
     """A spaCy v2.0 pipeline component that sets entity annotations
@@ -243,6 +243,81 @@ class YearMatcher(object):
 
 year_patterns = [{'ENT_TYPE': 'DATE', 'TAG' : 'CD', 'SHAPE' : 'dddd'}]
 
+class FalseDateMatcher(object):
+    """A spaCy pipeline component to flag arabic numbers if they 
+    include commas or are greater than 31. Its main use is to 
+    mitigate spaCy NER false positives."""
+    name = 'false_date'
+    
+    regex_pat = re.compile(r"^([4-9][\d]|3[2-9]|(([0-9]{1,3},)*[0-9]{3}([.][0-9])?))$")
+    
+    def __init__(self, nlp, pattern_list, match_id='FALSE_DATE', label='FALSE_DATE', regex_pat = regex_pat):
+        # register a new token extension to flag false_date tokens
+        
+        self.label = nlp.vocab.strings[label]  # get entity label ID
+        self.orig_label = nlp.vocab.strings['DATE']  # get entity label ID for date
+        Token.set_extension('is_false_date', default=False, force=True)
+        self.matcher = Matcher(nlp.vocab)
+        self.matcher.add(match_id, None, pattern_list)
+        self.regex_pat = regex_pat
+        
+        
+         # Register attributes on Doc and Span via a getter that checks if one of
+        # the contained tokens is set to is_false_date == True.
+        Doc.set_extension('has_false_date', getter=self.has_false_date, force=True)
+        Span.set_extension('has_false_date', getter=self.has_false_date, force=True)
+
+    def __call__(self, doc):
+        matches = self.matcher(doc)
+        candidate_spans = []  # collect the matched spans here
+        spans = [] # for tokens that match regex
+        for match_id, start, end in matches:
+            candidate_spans.append(doc[start:end])
+        for span in candidate_spans:
+#            span.merge()   # merge
+            for token in span:
+                if re.match(self.regex_pat, token.text):
+                    # Generate Span representing the entity & set label
+                    entity = Span(doc, token.i, token.i + 1, label=self.label)
+                    spans.append(entity)
+                    token._.is_false_date = True  # mark token as a false date
+                    # Get original date span
+                    orig_span = [e for e in doc.ents if token in e][0]
+                     # Create ents list to add to doc ents
+                    new_ents = []
+                    # re-run NER on rest of span
+                    if token.i > orig_span.start:
+                        left_span = doc[orig_span.start : token.i]
+                        left_ents = list(nlp(left_span.text).ents)
+                        if left_ents: 
+                            new_ents.append(Span(doc, left_span.start, left_span.end, label=self.orig_label))
+                    new_ents.append(entity)
+                    if token.i < orig_span.end:
+                        right_span = doc[token.i + 1 : orig_span.end + 1]
+                        right_ents = list(nlp(right_span.text).ents) 
+                        if right_ents:
+                            new_ents.append(Span(doc, right_span.start, right_span.end, label=self.orig_label))
+                    
+                     # Overwrite doc.ents and add entity – be careful not to replace!
+                    #doc.ents = list(doc.ents) + [entity]
+                    doc.ents = list(doc.ents) + new_ents
+            for span in spans:
+            # Iterate over all spans and merge them into one token. This is done
+            # after setting the entities – otherwise, it would cause mismatched
+            # indices!
+                span.merge()
+            
+        return doc
+    
+    def has_false_date(self, tokens):
+        """Getter for Doc and Span attributes. Returns True if one of the tokens
+        is a false date. Since the getter is only called when we access the
+        attribute, we can refer to the Token's 'is_false_date' attribute here,
+        which is already set in the processing step."""
+        return any([t._.get('is_false_date') for t in tokens])
+
+false_date_patterns = [{'ENT_TYPE': 'DATE', 'TAG' : 'CD'}]
+
 singles_word_list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 teens_word_list = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 tens_word_list = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
@@ -250,9 +325,6 @@ magnitude_word_list = ["hundred", "thousand", "million", "billion"]
 
 teen_unit_combos = [x.join([y,z])  for x in [" ", "-"] for y in get_case_combos(tens_word_list) for z in get_case_combos(singles_word_list) ]
 number_word_list = get_case_combos(singles_word_list) + get_case_combos(teens_word_list) + get_case_combos(tens_word_list) + get_case_combos(magnitude_word_list) + teen_unit_combos
-
-
-
 
 tens_dict = {}
 for w, n in zip([y + '-'  for y in tens_word_list ], list(range(2,10))):
@@ -266,7 +338,7 @@ for w, n in zip(singles_word_list, list(range(1,10))):
 
 nlp = spacy.load('en_core_web_lg')
 
-emp_noun_recognizer = EmpNounRecognizer(nlp, emp_terms_case_combos)
+emp_noun_recognizer = EmpNounRecognizer(nlp, emp_terms_list)
 nlp.add_pipe(emp_noun_recognizer, last=True) 
 
 emp_type_recognizer = EmpTypeRecognizer(nlp, emp_type_dict)
@@ -274,5 +346,9 @@ nlp.add_pipe(emp_type_recognizer, last=True)
 
 number_word_recognizer = NumberWordRecognizer(nlp, number_word_list)
 nlp.add_pipe(number_word_recognizer, last=True) 
+
 year_matcher = YearMatcher(nlp, year_patterns)
 nlp.add_pipe(year_matcher, last=True) 
+
+false_date_matcher = FalseDateMatcher(nlp, false_date_patterns)
+nlp.add_pipe(false_date_matcher, last=True) 
